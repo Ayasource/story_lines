c35312f12f9b05662f57de15b30bef7bc2c39c86
ayasource <ayana-aac@hotmail.co.uk>
2026-04-12 16:21:51 +0100

    Add Storyline model

--- story_lines/models.py
+++ story_lines/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone, User
 
 # Create your models here.
+
+
+class StoryLine(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='story_lines')
+    excerpt_name = models.CharField(max_length=200, unique=True)
+    story_date = models.DateTimeField(default=timezone.now)
+    story_summary = models.CharField(max_length=200)
+    story_excerpt = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
